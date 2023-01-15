e8df02e6c444d1e14e476eb22680d870d60f7b0f
Avesh Shaikh <97118756+AviiiSk@users.noreply.github.com>
2023-01-16 02:09:23 +0530

    Add files via upload
    subtraction program

---
+++ Subtraction.java
@@ -0,0 +1,32 @@
+
+// 2. Write the java program to design method for subtraction of two number,
+// which returns int value to that method and result should be print into main method.
+
+
+
+
+
+
+public class Subtraction {
+	
+	
+	@SuppressWarnings("unused")
+	public static void subtract(int a, int b)
+	{
+		int c  = a-b;
+		
+		System.out.println("Subtraction of Two Numbers : " + c);
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		
+		subtract(30, 15);
+		
+		
+
+	}
+
+}
